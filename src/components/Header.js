e200c4bacc99311e6473ba9bc53bc408e5894df5
mgcreations <info@mgcreations.co.za>
2021-09-26 19:56:02 +0200

    Changing basic template

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 
-const Header = (props) => (
-    <header id="header" className="alt">
-        <Link to="/" className="logo"><strong>Forty</strong> <span>by HTML5 UP</span></Link>
-        <nav>
-            <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
-        </nav>
-    </header>
+import logo from '../assets/images/logo.png'
+
+const Header = props => (
+  <header id="header" className="alt">
+    <Link to="/" className="logo">
+      <img src={logo} alt="Logo" />
+    </Link>
+    <nav>
+      <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">
+        Menu
+      </a>
+    </nav>
+  </header>
 )
 
 Header.propTypes = {
-    onToggleMenu: PropTypes.func
+  onToggleMenu: PropTypes.func,
 }
 
 export default Header
